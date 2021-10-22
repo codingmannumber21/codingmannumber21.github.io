7eb5114017fdf7edf1e8ace782fae0adb8ef4b46
codingmannumber21 <92760995+codingmannumber21@users.noreply.github.com>
2021-10-22 11:23:20 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function bruh(){
-        var username = document.getElementById("ok").value
-        window.alert(username)
-      }
+var a;
+
+function getItem(){
+        //get value from input field and set to a
+        a = document.getElementById("username").value;
+        //create local storage key with value of "a"
+        localStorage.setItem("uName", a);
+        //automatically take user to page 2.html
+        location.href = "two.html";
+}
