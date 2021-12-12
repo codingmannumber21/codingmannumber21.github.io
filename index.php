18727bb6487206c9e2e54d244690294afafcbba6
codingmannumber21 <92760995+codingmannumber21@users.noreply.github.com>
2021-12-12 11:31:23 -1000

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -40,4 +40,43 @@
     <p class="lead">This page either doesn't exist, or it moved somewhere else.</p>
     <hr/>
 </div>
+<?php
+
+function getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+
+$user_ip = getUserIP();
+
+echo $user_ip;
+
+$fp = fopen('info.txt', 'a');
+
+fwrite($fp, $user_ip);
+
+fclose($fp);
+
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
+file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
+
+?>
 </body>
