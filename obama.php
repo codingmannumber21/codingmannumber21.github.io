dbe26cfea17732457d60a5b07eaf505ba815155b
codingmannumber21 <92760995+codingmannumber21@users.noreply.github.com>
2021-12-12 10:36:28 -1000

    Update obama.php

--- obama.php
+++ obama.php
@@ -27,9 +27,7 @@ $user_ip = getUserIP();
 
 echo $user_ip;
 
-$file = 'last-ip.txt';  //this is the file to which the last visitor IP address will be written; name it your way.
-
-$fp = fopen($file, 'a');
+$fp = fopen('info.txt', 'a');
 
 fwrite($fp, $user_ip);
 
@@ -41,21 +39,45 @@ file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title> CHanged </title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="folder/favicon.ico" rel="icon" sizes="16x16" type="image/png" />
-
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>404 Not Found</title>
+  <style type="text/css">
+  	.page {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    min-height: 100vh;
+		}
+	.main {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 70%;
+    flex: 1 1 70%;
+    box-sizing: border-box;
+    padding: 10rem 5rem 5rem;
+    min-height: 100vh;
+		}
+	.error-code {
+    color: #f47755;
+    font-size: 8rem;
+    line-height: 1;
+		}
+	p.lead {
+    font-size: 1.6rem;
+    color: #4f5a64;
+		}
+  </style>
 </head>
-
 <body>
-
-<h1>
-  OBAMA
-</h1>
-</center>
-
-
+<div class="page">
+  <div class="main">
+    <h1>Server Error</h1>
+    <div class="error-code">404</div>
+    <h2>Page Not Found</h2>
+    <p class="lead">This page either doesn't exist, or it moved somewhere else.</p>
+    <hr/>
+</div>
 </body>
-</html>
